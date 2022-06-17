2043b653f997e6dc806bc46fc634a9750c9ae46f
Donray Williams <donrayxwilliams@gmail.com>
2022-06-17 12:11:42 -0500

    DEBT-23 | Adding User Authentication

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,9 @@
 import { Header } from './Header';
 import { Layout } from './Layout';
+import { AuthenticationModal } from './AuthenticationModal';
 
 export {
   Header,
   Layout,
+  AuthenticationModal,
 };
